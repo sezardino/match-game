f98bbe066f4a535681ddfa73693e0cf1b275a8c3
Edward <edward.arechwa@gogomedia.pl>
2021-07-07 11:40:43 +0200

    feat: add interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -28,3 +28,27 @@ export interface doSomethingArgs {
     obj: obj;
     property: keyof obj;
 }
+
+export interface IAction {
+    type: string;
+    payload: any;
+}
+
+export interface IGameState {
+    game: Boolean;
+    gamePolygon: any[] | null;
+    moves: any;
+    guessed: any;
+    timer: { id: any | null; time: number };
+    message: string;
+}
+
+export interface IUser {
+    userName: string;
+    userId: string;
+    score: number;
+}
+
+export interface IScoreState {
+    users: Array<IUser> | null;
+}
